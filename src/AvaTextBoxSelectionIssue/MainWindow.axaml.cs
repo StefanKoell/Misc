99b06ba4ea6939a87bfdd2f78607971e8b3bb8aa
Stefan Koell <stefan.koell@royalapps.com>
2025-02-12 17:14:16 +0100

    TextBox selection stuff

---
+++ src/AvaTextBoxSelectionIssue/MainWindow.axaml.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Threading;
+
+namespace AvaTextBoxSelectionIssue;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+            return;
+
+        Dispatcher.UIThread.Post(() =>
+        {
+            var popup = button.GetLogicalAncestors().OfType<Popup>().FirstOrDefault();
+            popup!.IsOpen = false;
+        });
+    }
+}
---
+++ src/AvaTextBoxSelectionIssue/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaTextBoxSelectionIssue;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public MainWindowViewModel()
+    {
+    }
+}
